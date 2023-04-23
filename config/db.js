016880d337cb4c9bb53f79135f48ceec6b3565ee
PriyanshShrivastava <learn.priyansh@gmail.com>
2023-04-24 00:31:23 +0530

    refactor: :bug: modify package.json and removed bug

--- config/db.js
+++ config/db.js
@@ -1,61 +1,17 @@
 const mongoose = require("mongoose");
-const csv = require("csv-parser");
-const fs = require("fs");
+require("dotenv").config();
 
-const url = process.env.MONGO_URL.replace(
-  "<PASSWORD>",
-  process.env.MONGO_PASSWORD
-);
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const connectDB = async () => {
+  const DB_URL = process.env.MONGO_URL.replace(
+    "<PASSWORD>",
+    process.env.MONGO_PASSWORD
+  );
+  try {
+    const conn = await mongoose.connect(DB_URL);
+    console.log(`Connected to mongo db database ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log("connected to MongoDB");
-
-  const data = [];
-
-  fs.createReadStream("data.csv")
-    .pipe(csv())
-    .on("data", (row) => {
-      // Remove any blank spaces or tabs from the row data
-      Object.keys(row).forEach((key) => (row[key] = row[key].trim()));
-
-      // Check if any columns are null or blank
-      Object.keys(row).forEach((key) => {
-        if (row[key] === null || row[key] === "") {
-          row[key] = undefined; // set to undefined to avoid validation errors
-        }
-      });
-
-      // Convert comma-separated values to array for "cast" and "listed_in" columns
-      if (row.cast) {
-        row.cast = row.cast.split(",").map((item) => item.trim());
-      }
-      if (row.listed_in) {
-        row.listed_in = row.listed_in.split(",").map((item) => item.trim());
-      }
-
-      data.push(row);
-    })
-    .on("end", () => {
-      // Insert data into MongoDB using insertMany() method
-      db.collection("movies").insertMany(data, (err, result) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log(`${result.insertedCount} documents inserted`);
-        }
-
-        // Close database connection when finished reading and saving data
-        db.close();
-      });
-    })
-    .on("error", (err) => {
-      console.error("Error reading CSV file:", err);
-    });
-});
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-import express from "express";
-import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
+const express = require("express");
+const connectDB = require("./config/db.js");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Databse connection
+connectDB();
+
+app.use("/api/v1/movies", movieRoute);
