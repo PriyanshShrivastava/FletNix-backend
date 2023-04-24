b43ed026cf973e5dd481252712b4357bbb0d14f9
PriyanshShrivastava <learn.priyansh@gmail.com>
2023-04-24 11:57:07 +0530

    feat: :fire: added a show model
    It will help fetching the data and also inserting data into db

---
+++ model/showModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const showSchema = new mongoose.Schema({
+  show_id: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: false,
+  },
+  cast: {
+    type: [String],
+    required: false,
+  },
+  country: {
+    type: String,
+    required: false,
+  },
+  date_added: {
+    type: Date,
+    required: false,
+  },
+  release_year: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: String,
+    required: false,
+  },
+  duration: {
+    type: String,
+    required: false,
+  },
+  listed_in: {
+    type: [String],
+    required: false,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("Show", showSchema);
